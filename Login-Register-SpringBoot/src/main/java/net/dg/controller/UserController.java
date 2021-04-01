aa79e093a6c79ac5dc7d7776bf6ff00964e78871
AlexDG <alexdg722@gmail.com>
2021-04-01 09:03:39 +0300

    Commit 01.04.2021

--- Login-Register-SpringBoot/src/main/java/net/dg/controller/UserController.java
+++ Login-Register-SpringBoot/src/main/java/net/dg/controller/UserController.java
@@ -3,6 +3,7 @@ package net.dg.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -32,7 +33,8 @@ public class UserController {
 	private EmailService emailService;
 	
 	@GetMapping("/login")
-	public String UserLogin() {
+	public String UserLogin(Model model) {
+	
 		return "login";
 	}
 	
